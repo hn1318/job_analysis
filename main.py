b22e7eb020b9eecb0123fb75f49f3223603ad321
JLHan <3080085013@qq.com>
2026-06-06 22:51:21 +0800

    初始

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from pyecharts.charts import Bar, Page, Pie, WordCloud
+from pyecharts import options as opts
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
